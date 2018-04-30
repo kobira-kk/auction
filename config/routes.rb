5d0fbd61f1b161bfb6fa2b07d86bed0b421685ee
kobira-kk <foxmancoffee@yahoo.co.jp>
2018-04-30 11:39:03 +0900

    登録・更新・削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  get '/items/:id' => 'items#show'
+  # 商品一覧
   get '/items' => 'items#index'
+
+  #  フォーム
+  get '/items/new' => 'items#new'
+  post'/items' => 'items#create'
+
+  # 商品詳細 一番下に記載しないと同一ネームに引っかかる :idは変数扱いのため
+  get '/items/:id' => 'items#show'
+
+  # 編集
+  get '/items/:id/edit' => 'items#edit' #追加
+  patch '/items/:id' => 'items#update'  #追加
+
+  delete '/items/:id' => 'items#destroy'
 end
